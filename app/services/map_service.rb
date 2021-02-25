1b23a3c7c016dad1bd1f000ed98c5c4fc09a820c
Cydnee Owens <cks410@yahoo.com>
2021-02-24 16:25:37 -0800

    Add place_search method and conn method
    Co-authored-by: Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>

--- app/services/map_service.rb
+++ app/services/map_service.rb
@@ -1,7 +1,21 @@
+require 'faraday'
+
 class MapService 
   class << self
-    def method_name
-      
+    def place_search(query)
+      require 'pry'; binding.pry
+      response = conn.get('place/textsearch/json') do |req|
+        req.params[:query] = query.downcase.gsub(' ', '-')
+      end
+      JSON.parse(response.body, symbolize_names: true)
+    end
+
+    private
+
+    def conn
+      @conn ||= Faraday.new('https://maps.googleapis.com/maps/api') do |req|
+        req.params[:key] = ENV['MAP_API_KEY']
+      end
     end
   end
 end
