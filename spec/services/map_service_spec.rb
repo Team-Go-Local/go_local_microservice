4b1d6ebf1ef4f70e02dc19bb01aacaea80d1a7fd
Saundra <saundracatalina@gmail.com>
2021-02-25 18:26:49 -0700

    Add VCR cassette call to gets place info data test
    Co-authored-by: Isabelle Villasenor <45639675+isabellevillasenor@users.noreply.github.com>

--- spec/services/map_service_spec.rb
+++ spec/services/map_service_spec.rb
@@ -2,27 +2,29 @@ require 'spec_helper'
 
 RSpec.describe MapService do
   it 'gets place info data' do
-    query_string = 'Casa Bonita Denver'
-    place_info   = MapService.place_search(query_string)
+    VCR.use_cassette('place_search') do
+      query_string = 'Casa Bonita Denver'
+      place_info   = MapService.place_search(query_string)
 
-    expect(place_info).to be_a Hash
-    check_hash_structure(place_info, :results, Array)
-    expect(place_info[:results][0]).to be_a Hash
+      expect(place_info).to be_a Hash
+      check_hash_structure(place_info, :results, Array)
+      expect(place_info[:results][0]).to be_a Hash
 
-    place = place_info[:results][0]
-    
-    check_hash_structure(place, :business_status, String)
-    check_hash_structure(place, :formatted_address, String)
-    check_hash_structure(place, :geometry, Hash)
-    check_hash_structure(place, :icon, String)
-    check_hash_structure(place, :name, String)
-    check_hash_structure(place, :photos, Array)
-    check_hash_structure(place, :place_id, String)
-    check_hash_structure(place, :plus_code, Hash)
-    check_hash_structure(place, :price_level, Integer)
-    check_hash_structure(place, :rating, Float)
-    check_hash_structure(place, :reference, String)
-    check_hash_structure(place, :types, Array)
-    check_hash_structure(place, :user_ratings_total, Integer)
+      place = place_info[:results][0]
+
+      check_hash_structure(place, :business_status, String)
+      check_hash_structure(place, :formatted_address, String)
+      check_hash_structure(place, :geometry, Hash)
+      check_hash_structure(place, :icon, String)
+      check_hash_structure(place, :name, String)
+      check_hash_structure(place, :photos, Array)
+      check_hash_structure(place, :place_id, String)
+      check_hash_structure(place, :plus_code, Hash)
+      check_hash_structure(place, :price_level, Integer)
+      check_hash_structure(place, :rating, Float)
+      check_hash_structure(place, :reference, String)
+      check_hash_structure(place, :types, Array)
+      check_hash_structure(place, :user_ratings_total, Integer)
+    end
   end
 end
