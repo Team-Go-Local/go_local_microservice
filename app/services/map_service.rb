3805f0868309e46f4e89a06c95d7eac28ef7b1c8
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-02-27 16:10:02 -0700

    add route and service method for place details endpoint

--- app/services/map_service.rb
+++ app/services/map_service.rb
@@ -3,6 +3,16 @@ class MapService
     def place_search(query)
       response = conn.get('place/findplacefromtext/json') do |req|
         req.params[:input] = query
+        req.params[:inputtype] = 'textquery'
+        req.params[:fields] = 'place_id,formatted_address,name,types'
+      end
+      JSON.parse(response.body, symbolize_names: true)
+    end
+
+    def place_details(place_id)
+      response = conn.get('place/details/json') do |req|
+        req.params[:place_id] = place_id
+        req.params[:fields] = 'name,formatted_address,formatted_phone_number,opening_hours/weekday_text,website,types,business_status'
       end
       JSON.parse(response.body, symbolize_names: true)
     end
@@ -12,8 +22,6 @@ class MapService
     def conn
       @conn ||= Faraday.new('https://maps.googleapis.com/maps/api') do |req|
         req.params[:key] = ENV['MAP_API_KEY']
-        req.params[:inputtype] = 'textquery'
-        req.params[:fields] = 'place_id,formatted_address,name,types'
       end
     end
   end
