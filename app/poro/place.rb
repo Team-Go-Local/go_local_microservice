8992653f30b4f02b527a5a4885e20c0d9184038f
Isabelle Villasenor <villasenori@outlook.com>
2021-02-26 17:44:08 -0700

    Add Place PORO Spec and PORO
    Co-authored-by: Saundra Catalina <68261312+saundracatalina@users.noreply.github.com>

---
+++ app/poro/place.rb
@@ -0,0 +1,17 @@
+require 'sinatra/base'
+require './config/environment'
+require './app/services/map_service'
+
+class Place
+  attr_reader :place_id, 
+              :formatted_address, 
+              :name, 
+              :type
+
+  def initialize(result)
+    @place_id = result[:results][0][:place_id]
+    @formatted_address = result[:results][0][:formatted_address]
+    @name = result[:results][0][:name]
+    @type = result[:results][0][:type]
+  end
+end
---
+++ spec/poros/place_poro_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+RSpec.describe Place do
+  before :each do 
+    @result = {"results": [{ "formatted_address": "6715 W Colfax Ave, Lakewood, CO 80214, United States",
+              "name": "Casa Bonita",
+              "place_id": "ChIJE7tYRySHa4cRSauU_fDROfk",
+              "types": [
+                "restaurant",
+                "food",
+                "point_of_interest",
+                "establishment"
+                ]}]}
+
+      @casa = Place.new(@result)
+    end
+
+  it 'instantiates with attributes' do
+    expect(@casa).to be_a Place
+    expect(@casa.place_id).to eq(@result[:results][0][:place_id])
+    expect(@casa.formatted_address).to eq(@result[:results][0][:formatted_address])
+    expect(@casa.name).to eq(@result[:results][0][:name])
+    expect(@casa.type).to eq(@result[:results][0][:type])
+  end
+end
