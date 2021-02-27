904f26dcc0db11a7198e2688d14fbcebed047b60
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-02-27 15:21:53 -0700

    add tests to account for closed businesses with no operating hours

--- spec/services/map_service_spec.rb
+++ spec/services/map_service_spec.rb
@@ -43,8 +43,8 @@ RSpec.describe MapService do
   describe 'place details' do
     it 'gets place info' do
       VCR.use_cassette('place_details') do
-        place_id = 'ChIJE7tYRySHa4cRSauU_fDROfk'
-        place_info   = MapService.place_details(place_id)
+        place_id = 'ChIJFaqhMyt_bIcRMfeTGF4E8kM'
+        place_info = MapService.place_details(place_id)
 
         expect(place_info).to be_a(Hash)
         check_hash_structure(place_info, :status, String)
@@ -54,11 +54,36 @@ RSpec.describe MapService do
         check_hash_structure(place_info[:result], :formatted_phone_number, String)
         check_hash_structure(place_info[:result], :name, String)
         check_hash_structure(place_info[:result], :website, String)
+        check_hash_structure(place_info[:result], :business_status, String)
+        expect(place_info[:result][:business_status]).to eq('OPERATIONAL')
+        check_hash_structure(place_info[:result], :opening_hours, Hash)
+        check_hash_structure(place_info[:result][:opening_hours], :weekday_text, Array)
+        all_strings = place_info[:result][:opening_hours][:weekday_text].all? do |type|
+          type.class == String
+        end
+        expect(all_strings).to be true
         check_hash_structure(place_info[:result], :types, Array)
-        all_strings = place_info[:result][:types].all? { |type| type.class == String }
+        all_strings = place_info[:result][:types].all? do |type|
+          type.class == String
+        end
         expect(all_strings).to be true
         expect(place_info.keys).to match_array(%i[html_attributions result status])
-        expect(place_info[:result].keys).to match_array(%i[formatted_address formatted_phone_number name types website])
+        expect(place_info[:result].keys).to match_array(%i[formatted_address formatted_phone_number name types website business_status opening_hours])
+      end
+    end
+
+    it 'does not get hours if a business is not operational' do
+      VCR.use_cassette('closed_business') do
+        place_id = 'ChIJE7tYRySHa4cRSauU_fDROfk'
+        place_info = MapService.place_details(place_id)
+
+        expect(place_info).to be_a(Hash)
+        check_hash_structure(place_info, :status, String)
+        expect(place_info[:status]).to eq('OK')
+        check_hash_structure(place_info[:result], :business_status, String)
+        expect(place_info[:result][:business_status]).to eq('CLOSED_TEMPORARILY')
+        expect(place_info[:result]).not_to have_key(:opening_hours)
+        expect(place_info[:result].keys).to match_array(%i[formatted_address formatted_phone_number name types website business_status])
       end
     end
   end
