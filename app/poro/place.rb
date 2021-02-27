bcc03177e7e375a7802c8e0857ed465403861410
Isabelle Villasenor <villasenori@outlook.com>
2021-02-26 21:30:20 -0700

    Add nil ID to Place PORO for fast_json
    Co-authored-by: Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>

--- app/poro/place.rb
+++ app/poro/place.rb
@@ -6,9 +6,11 @@ class Place
   attr_reader :place_id, 
               :formatted_address, 
               :name, 
-              :type
+              :type,
+              :id
 
   def initialize(result)
+    @id = nil
     @place_id = result[:results][0][:place_id]
     @formatted_address = result[:results][0][:formatted_address]
     @name = result[:results][0][:name]
