1432e00b0fd1b56b247057e0356425422d4d2e4e
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-02-27 14:21:27 -0700

    change specs to look for specific fields in google api response

--- spec/poros/place_poro_spec.rb
+++ spec/poros/place_poro_spec.rb
@@ -2,24 +2,21 @@ require 'spec_helper'
 
 RSpec.describe Place do
   before :each do
-    @result = {"results": [{ "formatted_address": "6715 W Colfax Ave, Lakewood, CO 80214, United States",
-              "name": "Casa Bonita",
-              "place_id": "ChIJE7tYRySHa4cRSauU_fDROfk",
-              "types": [
-                "restaurant",
-                "food",
-                "point_of_interest",
-                "establishment"
-                ]}]}
+    @result = {
+      formatted_address: "6715 W Colfax Ave, Lakewood, CO 80214, United States",
+      name: "Casa Bonita",
+      place_id: "ChIJE7tYRySHa4cRSauU_fDROfk",
+      types: ["restaurant", "food", "point_of_interest", "establishment"]
+    }
 
     @casa = Place.new(@result)
   end
 
   it 'instantiates with attributes' do
     expect(@casa).to be_a Place
-    expect(@casa.place_id).to eq(@result[:results][0][:place_id])
-    expect(@casa.formatted_address).to eq(@result[:results][0][:formatted_address])
-    expect(@casa.name).to eq(@result[:results][0][:name])
-    expect(@casa.types).to eq(@result[:results][0][:types])
+    expect(@casa.place_id).to eq(@result[:place_id])
+    expect(@casa.formatted_address).to eq(@result[:formatted_address])
+    expect(@casa.name).to eq(@result[:name])
+    expect(@casa.types).to eq(@result[:types])
   end
 end
--- spec/services/map_service_spec.rb
+++ spec/services/map_service_spec.rb
@@ -9,24 +9,15 @@ RSpec.describe MapService do
       expect(place_info).to be_a Hash
       check_hash_structure(place_info, :status, String)
       expect(place_info[:status]).to eq('OK')
-      check_hash_structure(place_info, :results, Array)
-      expect(place_info[:results][0]).to be_a Hash
+      check_hash_structure(place_info, :candidates, Array)
+      expect(place_info[:candidates][0]).to be_a Hash
 
-      place = place_info[:results][0]
+      place = place_info[:candidates][0]
 
-      check_hash_structure(place, :business_status, String)
       check_hash_structure(place, :formatted_address, String)
-      check_hash_structure(place, :geometry, Hash)
-      check_hash_structure(place, :icon, String)
       check_hash_structure(place, :name, String)
-      check_hash_structure(place, :photos, Array)
       check_hash_structure(place, :place_id, String)
-      check_hash_structure(place, :plus_code, Hash)
-      check_hash_structure(place, :price_level, Integer)
-      check_hash_structure(place, :rating, Float)
-      check_hash_structure(place, :reference, String)
       check_hash_structure(place, :types, Array)
-      check_hash_structure(place, :user_ratings_total, Integer)
     end
   end
 
@@ -38,8 +29,8 @@ RSpec.describe MapService do
       expect(place_info).to be_a Hash
       check_hash_structure(place_info, :status, String)
       expect(place_info[:status]).to eq('ZERO_RESULTS')
-      check_hash_structure(place_info, :results, Array)
-      expect(place_info[:results]).to be_empty
+      check_hash_structure(place_info, :candidates, Array)
+      expect(place_info[:candidates]).to be_empty
     end
   end
 end
